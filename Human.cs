5a87475f916ffb98f44beaeb2449f6d745e9f29f
marinarediko <ma.rediko@gmail.com>
2020-06-15 17:36:28 +0300

    Add project files.

---
+++ Human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppLearning
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string EyeColor { get; set; }
+        public Human()
+        {
+
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, my full name is " + this.Name + " " + this.Surname + ". " + "I am " + this.Age + " years old." );
+        }
+
+        public Human(string n, string s)
+        {
+            Name = n;
+            Surname = s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person;
+            person = new Human();
+            //Console.WriteLine("Enter your name");
+            //person.Name = Console.ReadLine();
+            person.Name = "Jinny";
+            person.Surname = "Allrey";
+            person.Age = 36;
+            person.EyeColor = "Blue";
+            person.Introduce();
+
+            person.Name = "Anna";
+            person.Introduce();
+
+            Human person1 = new Human("Marina", "Rediko");
+            Console.WriteLine("Hello, {0}", person1.Name);
+            
+        }
+    }
+}
